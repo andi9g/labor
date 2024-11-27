9699e77b5e377badecfe4bfd1066d8cdb0fb0e9c
andi9g <andirizky.bayuputra@gmail.com>
2024-11-27 16:27:23 +0700

    default String Length

--- routes/api.php
+++ routes/api.php
@@ -16,11 +16,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('login/username', [AuthController::class, 'loginWithUsername']);
-Route::post('login/token', [AuthController::class, 'loginWithToken']);
+Route::group([
 
-Route::middleware('auth:api')->group(function () {
-    Route::get("data", [HomeController::class, 'message']);
-    // Route::post('logout', [AuthController::class, 'logout'])->name("logout");
-    // Route::get('me', [AuthController::class, 'me']);
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('login/username', [AuthController::class, 'loginWithUsername']);
+    Route::post('login/token', [AuthController::class, 'loginWithToken']);
+
+    Route::middleware('auth:api')->group(function () {
+        Route::get("data", [HomeController::class, 'message']);
+        // Route::post('logout', [AuthController::class, 'logout'])->name("logout");
+        // Route::get('me', [AuthController::class, 'me']);
+    });
+
 });
